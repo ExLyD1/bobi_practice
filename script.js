73f56e88b23177f4aa1605ab157e1aeae4b8ac5b
ExLyD1 <sashaahnij@gmail.com>
2024-04-20 21:26:33 +0300

    more JS functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
@@ -8,4 +8,50 @@ new Swiper('.swiper-container', {
     clickable: true,
     dynamicBullets: true,
   },
+});
+
+const solutionsSelectors = document.querySelectorAll(".solSelHolder .solSelectors");
+
+solutionsSelectors.forEach(element => {
+  element.addEventListener("click", event => {
+    event.target.classList.add("solSelectorsCol");
+    solutionsSelectors.forEach(otherElement => {
+      if (otherElement !== event.target) {
+        otherElement.classList.remove("solSelectorsCol");
+      }
+    });
+  });
+});
+
+swiper.on('slideChange', () => {
+  const activeSlideIndex = swiper.realIndex;
+  const solutionsSelectors = document.querySelectorAll(".solSelHolder .solSelectors");
+  solutionsSelectors.forEach(element => {
+    element.classList.remove("solSelectorsCol");
+  });
+  solutionsSelectors[activeSlideIndex].classList.add("solSelectorsCol");
+});
+
+solutionsSelectors.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    swiper.slideTo(index);
+  });
+});
+
+
+
+
+const itemHolder = document.querySelectorAll(".selItemHolder .selItem")
+
+itemHolder.forEach(element => {
+  element.addEventListener("click", event => {
+    const parent = event.target.closest(".selItem")
+    parent.classList.add("selItemCol")
+
+    itemHolder.forEach(otherElement => {
+      if (otherElement!==parent) {
+      otherElement.classList.remove("selItemCol")
+    }
+    })
+  })
 });
